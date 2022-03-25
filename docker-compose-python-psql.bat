bf89afd5fc1cd0ac43700cf130ded3f2fa521ff9
Nuno Antunes <nmsa@dei.uc.pt>
2022-03-25 20:02:26 +0000

    Fix python issues of T class (#3)
    * Add .bat files for windows users.
    
    * Add logs folder. This folder should exist for the python version to work.

---
+++ docker-compose-python-psql.bat
@@ -0,0 +1,25 @@
+:: !/bin/bash
+:: 
+::  Bases de Dados 2021/2022
+::  Trabalho Prático
+:: 
+::  Authors:
+::    Nuno Antunes <nmsa@dei.uc.pt>
+::    BD 2022 Team - https://dei.uc.pt/lei/
+::    University of Coimbra
+
+
+:: :: 
+:: ::  ATTENTION: This will stop and delete all the running containers
+:: ::  Use it only if you are not using docker for other ativities
+:: ::  
+:: docker rm $(docker stop $(docker ps -a -q))
+
+
+
+
+::  add  -d  to the command below if you want the containers running in background without logs
+docker-compose  -f docker-compose-python-psql.yml up --build
+
+
+
--- docker-compose-python-psql.sh
+++ docker-compose-python-psql.sh
@@ -15,7 +15,8 @@
 #
 #docker rm $(docker stop $(docker ps -a -q))
 
-mkdir -p python/app/logs
+
+
 
 # add  -d  to the command below if you want the containers running in background without logs
 docker-compose  -f docker-compose-python-psql.yml up --build
